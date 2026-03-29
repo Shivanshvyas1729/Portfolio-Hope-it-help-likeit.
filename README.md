2e85b8a091110e56850fec43b6ec47e53bcd980c
shivaji <shivanshvyas1729@gmail.com>
2026-03-30 03:45:10 +0530

    dict added

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Welcome to your Lovable project
+# Welcome to your portfolio  project
 
 TODO: Document your project here
 
-src->dataprotfolio.yaml -> all change u do there
+
+
+fork the repo 
+make folder in your system open cmd intilize git (git init)-> git clone url(from download option in github)
+or
+download as a zip and extract 
+
+all change u will do in
+src->dataprotfolio.yaml (src\data\portfolio.yaml)-> all change u do there
+
+and run locally using npm run dev 
+run on netilfy all file are already intialized for it 
+make steps there->only give this other ignore and u can give your api there option 
+Publish directory: dict
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,25 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
+//   },
+// }));
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true, // ✅ fix
     port: 8080,
     hmr: {
       overlay: false,
